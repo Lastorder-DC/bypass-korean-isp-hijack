04e14695212a3f9f3276f632d9f7b76b47572f3b
Lastorder <lastorder@yjsoft.xyz>
2018-01-02 12:35:26 +0900

    Upload userscript

---
+++ bypasshijack.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         ByPass Korean ISP Hijack
+// @namespace    http://lastorder.xyz/
+// @version      0.1
+// @description  ByPass Korean ISP Hijack
+// @author       Lastorder_DC
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var HijackIP = ["59.29.251.41"];
+
+    for(var i = 0;i < document.getElementsByTagName("script").length; i++) {
+        for(var j = 0;j < HijackIP.length; j++) {
+            if(document.getElementsByTagName("script")[i].innerHTML.indexOf(HijackIP[j]) !== -1) {
+                console.log("ISP Hijack detected. Reloading...");
+                location.reload();
+            }
+        }
+    }
+})();
