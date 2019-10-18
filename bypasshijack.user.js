6050277c04a9d6bbc5524d1ef36b5ec3459b0262
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2019-10-18 15:55:02 +0900

    Add SKT IP & Version up

--- bypasshijack.user.js
+++ bypasshijack.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ByPass Korean ISP Hijack
 // @namespace    http://lastorder.xyz/
-// @version      0.1
+// @version      0.2
 // @description  ByPass Korean ISP Hijack
 // @author       Lastorder_DC
 // @match        http://*/*
